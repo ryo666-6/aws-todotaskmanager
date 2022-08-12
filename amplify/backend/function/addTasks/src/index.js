a70214ece97913cbe3ee254346f9389f94fe9b54
Ryo <ryo19965intherock@gmail.com>
2022-08-12 15:51:17 +0900

    add code into task-dev

--- amplify/backend/function/addTasks/src/index.js
+++ amplify/backend/function/addTasks/src/index.js
@@ -3,6 +3,7 @@
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
     return {
--- amplify/backend/function/tasks/src/index.js
+++ amplify/backend/function/tasks/src/index.js
@@ -1,17 +1,31 @@
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+  host: 'database-1.cy7qik1duxfi.ap-northeast-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'password',
+  database: 'taskmanager'
+});
 
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
-exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+const response = {
+  statusCode: 200,
+  headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*"
+  }, 
+  body: ""
+};
+
+exports.handler = (event, context, callback) => {
+  const sql = 'select * from taskmanager.tasks;';
+
+  connection.query(sql, function(err, result) {
+    if (err) {
+      console.log(err);
+      return err;
+    }
+    response['body'] = JSON.stringify(result)
+    callback(null, response);
+  });
+  connection.end();
 };
