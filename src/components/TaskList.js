3c767ffa6fd50cd1d3d057bb20a10190586ed936
Ryo <ryo19965intherock@gmail.com>
2022-08-20 12:05:13 +0900

    change edit gettasklist

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -100,7 +100,7 @@ class TaskList extends React.Component {
 
     onDeleteClick = async (taskid) => {
         axios.delete(API_URL + `/deleteTasks/${taskid}`)
-        await new Promise(resolve => setTimeout(resolve, 3000));
+        await new Promise(resolve => setTimeout(resolve, 1000));
         this.getTaskList()
     }
 
